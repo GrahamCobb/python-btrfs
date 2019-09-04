GrahamCobb/python-btrfs

commit c7cb45def88bcd51b3a6bb168d295b535b316556

Retrieve Device Stats
The ioctl to retrieve device stats was introduced in kernel commit
c11d2c23 and changed a bit in b27f7c0c1 shortly after that.

The call uses a buffer which is slightly larger than 1kB, because of
adding a field in the second commit, while not updating the padding.

The internal implementation is a bit overengineered, designed for
maximum flexibility and future proof forward and backward compatibility.
It never changed since it exists, so I'm just hardcoding the five
current stats that are returned for a device. As soon something gets
added in the future, it will be early enough to start making it a little
bit more flexible.

    -# ./fs_info.py /mnt/extents/
    max_id 2 num_devices 2 fsid f32900b2-d23d-46e8-95b5-b5bdf70de9b1
        nodesize 16384 sectorsize 4096 clone_alignment 4096
    devid 1 uuid 019dfc23-20c4-4c9f-970a-053c78624edd bytes_used 1375731712
        total_bytes 26843545600 path /dev/mapper/durka-extents
    devid 1 write_errs 0 read_errs 0 flush_errs 0 generation_errs 0 corruption_errs 0
    devid 2 uuid e2741672-3dd8-407e-aa11-a8686cb31f50 bytes_used 0
        total_bytes 10737418240 path /dev/mapper/durka-extents2
    devid 2 write_errs 0 read_errs 0 flush_errs 0 generation_errs 0 corruption_errs 0

Using the reset option, stats can be reset, just like btrfs dev stats -z
would do.

btrfs/ioctl.py
@@ -119,6 +119,30 @@ class DevInfo(object):
             self.devid, self.uuid, self.bytes_used, self.total_bytes, self.path)
 
 
+ioctl_get_dev_stats = struct.Struct("=QQQ5Q968x")
+IOC_GET_DEV_STATS = _IOWR(BTRFS_IOCTL_MAGIC, 52, ioctl_get_dev_stats)
+
+
+class DevStats(object):
+    def __init__(self, buf):
+        self.devid, self.nr_items, self.flags, self.write_errs, self.read_errs, \
+            self.flush_errs, self.generation_errs, self.corruption_errs = \
+            ioctl_get_dev_stats.unpack_from(buf)
+
+    def __str__(self):
+        return "devid {0} write_errs {1} read_errs {2} flush_errs {3} generation_errs {4} " \
+            "corruption_errs {5}".format(self.devid, self.write_errs, self.read_errs,
+                                         self.flush_errs, self.generation_errs,
+                                         self.corruption_errs)
+
+
+def dev_stats(fd, devid, reset=False):
+    buf = create_buf(ioctl_get_dev_stats.size)
+    ioctl_get_dev_stats.pack_into(buf, 0, devid, 5, int(reset), 0, 0, 0, 0, 0)
+    fcntl.ioctl(fd, IOC_GET_DEV_STATS, buf)
+    return DevStats(buf)
+
+
 ioctl_space_args = struct.Struct("=2Q")
 ioctl_space_info = struct.Struct("=3Q")
 IOC_SPACE_INFO = _IOWR(BTRFS_IOCTL_MAGIC, 20, ioctl_space_args)

btrfs/ctree.py
@@ -280,6 +280,9 @@ class FileSystem(object):
     def dev_info(self, devid):
         return btrfs.ioctl.DevInfo(self.fd, devid)
 
+    def dev_stats(self, devid, reset=False):
+        return btrfs.ioctl.dev_stats(self.fd, devid, reset)
+
     def space_info(self):
         return btrfs.ioctl.space_info(self.fd)
 

examples/fs_info.py
@@ -9,3 +9,4 @@ fs_info = fs.fs_info()
 print(fs_info)
 for device in fs.devices():
     print(fs.dev_info(device.devid))
+    print(fs.dev_stats(device.devid))

commit be20b2f88a74478adf02f861f15fea8186ff4600

ioctl: fix dev_stats struct field order
I mixed up two fields in the dev_stats struct when initially writing this
code.

Reported-By: Kepi
Fixes: c7cb45def88b ("Retrieve Device Stats")
Closes: https://github.com/knorrie/python-btrfs/issues/21

btrfs/ioctl.py
@@ -228,7 +228,7 @@ class DevStats(object):
     """
     def __init__(self, buf):
         self.devid, self.nr_items, self.flags, self.write_errs, self.read_errs, \
-            self.flush_errs, self.generation_errs, self.corruption_errs = \
+            self.flush_errs, self.corruption_errs, self.generation_errs = \
             ioctl_get_dev_stats.unpack_from(buf)
 
     @property
@@ -237,15 +237,15 @@ class DevStats(object):
             'write_errs': self.write_errs,
             'read_errs': self.read_errs,
             'flush_errs': self.flush_errs,
-            'generation_errs': self.generation_errs,
             'corruption_errs': self.corruption_errs,
+            'generation_errs': self.generation_errs,
         }
 
     def __str__(self):
-        return "devid {0} write_errs {1} read_errs {2} flush_errs {3} generation_errs {4} " \
-            "corruption_errs {5}".format(self.devid, self.write_errs, self.read_errs,
-                                         self.flush_errs, self.generation_errs,
-                                         self.corruption_errs)
+        return "devid {0} write_errs {1} read_errs {2} flush_errs {3} corruption_errs {4} " \
+            "generation_errs {5}".format(self.devid, self.write_errs, self.read_errs,
+                                         self.flush_errs, self.corruption_errs,
+                                         self.generation_errs)
 
 
 def dev_stats(fd, devid, reset=False):